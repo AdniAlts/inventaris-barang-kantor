8c7a9d9cf1d4f6635a01584f2304559a374f02bb
Fathoni <88326103+thoni08@users.noreply.github.com>
2025-05-30 19:16:40 +0700

    feat: pengambilan nama kategori dan kondisi sesuai database sehingga dinamis

---
+++ src/utils/get_names.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * File: get_names.php
+ * Deskripsi: Modul ini bertanggung jawab untuk pengambilan nama untuk filter yang ada pada database.
+ * Dapat digunakan sebagai tampilan atau penentuan kondisi.
+ */
+require_once "../config/db.php";
+
+/**
+ * Fungsi `getCategoryNames()`:
+ * Berfungsi dalam pengambilan semua kategori barang dari database.
+ *
+ * @return array Array berisi data kategori barang yang telah di atur pada database.
+ */
+function getCategoryNames() {
+  $db = new db();
+  $categories = [];
+  $sql = "SELECT id_kategori, nama FROM kategori";
+  $result = $db->conn->query($sql);
+
+  if ($result) {
+    if ($result->num_rows > 0) {
+      while ($row = $result->fetch_assoc()) {
+        $categories[] = $row;
+      }
+    }
+  } else {
+    error_log("Gagal mengambil kategori dari database. Error: " . $db->conn->error);
+  }
+
+  $db->conn->close();
+  return $categories;
+}
+
+/**
+ * Fungsi `getStateNames()`:
+ * Berfungsi dalam pengambilan semua state barang dari database.
+ *
+ * @return array Array berisi data state yang telah di atur pada database.
+ */
+function getStateNames() {
+  $db = new db();
+  $states = [];
+  $sql = "SELECT id_state, nama FROM state";
+  $result = $db->conn->query($sql);
+
+  if ($result) {
+    if ($result->num_rows > 0) {
+      while ($row = $result->fetch_assoc()) {
+        $states[] = $row;
+      }
+    }
+  } else {
+    error_log("Gagal mengambil state dari database. Error: " . $db->conn->error);
+  }
+
+  $db->conn->close();
+  return $states;
+}
+?>
