dce2668c6b43217441b1998961672d5d486f0696
Adni <abubakaradni16@gmail.com>
2025-06-02 17:30:54 +0700

    fix modal tambah barang

--- src/pages/barang.php
+++ src/pages/barang.php
@@ -197,6 +197,7 @@ $db->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel - Manajemen Barang</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
     <style>
         body {
